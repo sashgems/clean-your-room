2c2517562a9975359d70c8bd7f937df48e4ecf71
Sasha Gemis <sashgems@sashas-air.andoverma.guest>
2019-10-01 12:59:06 -0400

    see code more clearly

--- pricing.js
+++ pricing.js
@@ -1,36 +1,49 @@
-module.exports.calculateProductPrice = function (product, employee, coverageLevels) {
-  var price = 0
+
+function calculatevolLife (coverageLevels, price, product){
+
+  for (var i = 0; i < coverageLevels.length; i++) {
+  var coverageAmount = coverageLevels[i].coverage
+
+  price += (coverageAmount / product.cost.costDivisor) * product.cost.price
+}
+
+if (product.employerContribution.mode === 'dollar') {
+  price = price - product.employerContribution.contribution
+} else {
+  dollarsOff = price * (product.employerContribution.contribution / 100)
+  price = price - dollarsOff
+}
+
+return parseInt(price * 100) / 100
+}
+
+function calculateltd (product, price, employee) { 
+  
+  var salaryPercentage = product.coveragePercentage / 100
+
+  price += ((employee.salary * salaryPercentage) / product.cost.costDivisor) * product.cost.price
+  
+  if (product.employerContribution.mode === 'dollar') {
+    price = price - product.employerContribution.contribution
+  } else {
+    dollarsOff = price * product.employerContribution.contribution
+    price = price - dollarsOff
+  }
+  
+  return parseInt(price * 100) / 100
+
+}
+
+module.exports.calculateProductPrice = function (product,employee, coverageLevels) {
+
+  var price = 0 
   var dollarsOff = 0
 
   switch (product.type) {
     case 'volLife':
-      for (var i = 0; i < coverageLevels.length; i++) {
-        var coverageAmount = coverageLevels[i].coverage
-
-        price += (coverageAmount / product.cost.costDivisor) * product.cost.price
-      }
-
-      if (product.employerContribution.mode === 'dollar') {
-        price = price - product.employerContribution.contribution
-      } else {
-        dollarsOff = price * (product.employerContribution.contribution / 100)
-        price = price - dollarsOff
-      }
-
-      return parseInt(price * 100) / 100
+      return calculatevolLife (coverageLevels, price, product)
     case 'ltd':
-      var salaryPercentage = product.coveragePercentage / 100
-
-      price += ((employee.salary * salaryPercentage) / product.cost.costDivisor) * product.cost.price
-
-      if (product.employerContribution.mode === 'dollar') {
-        price = price - product.employerContribution.contribution
-      } else {
-        dollarsOff = price * product.employerContribution.contribution
-        price = price - dollarsOff
-      }
-
-      return parseInt(price * 100) / 100
+      return calculateltd (product, price, employee)
     default:
       return 0
   }
